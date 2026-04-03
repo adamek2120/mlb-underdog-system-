e2e7ce74d059e96b30c97f026a8e8fb6a8ce06fe
John F. Adamek <58127303+adamek2120@users.noreply.github.com>
2026-04-03 14:40:09 -0400

    Update Full_System26.R
    Updated code with the Full_System26_CondensedWorking.R that adds in the Petricho criteria, removed the previous home dog picks and includes a team predictive ranking that matches the rating

--- Scripts/Full_System26.R
+++ Scripts/Full_System26.R
@@ -57,34 +57,15 @@ today_merged <- today_dog %>%
          "Fav_diff" = "diff") %>%
   select(-c(ra,rs,wins,losses,away))
 
-########################
-# Road Dog Criteria
-########################
-
-# 1) Start with Road Dogs					
-# 2) Favorite must not have a winning streak of 3 or more games	(FLAG_strk)				
-# 3) Dog must not be on a losing streak of 3 or more games	(FLAG_strk)				
-# 4) Dog must be at least 4-6 in L10 (FLAG_L10) and at least .400 on the road (FLAG_Away)				
-# 5) Favorite must not be better then 6-4 in L10 (FLAG_L10)
-
-
-#########################
-# Today's System Picks
-#########################
-crit1_today <- today_merged %>% 
-  mutate(FLAG_strk = case_when((Dog_streak == "wins" | (Dog_streak == "losses" & Dog_streakN < 3)) &
-                                 (Fav_streak == "losses" |(Fav_streak == "wins" & Fav_streakN < 3))  ~ "Met",
-                               .default = "Not-Met"),
-         FLAG_L10 = case_when(as.numeric(Dog_L10) >= .400 & as.numeric(Fav_L10) <= .600 ~ "Met",
-                              .default = "Not-Met"),
-         FLAG_Away = case_when(as.numeric(Dog_away_per) >= .400 ~ "Met",
-                               .default = "Not-Met"))
-
-
 ##########################
-# Tracking (teamranking)
+# Teamranking Data
 ###########################
 
+# Add's Rank to rating
+pred <- pred %>%
+  mutate(Pred_Rank = row_number())
+
+# Cleans & merges code
 tr_merge <- alldogs %>%
   left_join(todays_schedule, by = c("away_team" = "AwayTeam")) %>%
   rename("Dog" = "outcomes_name",
@@ -106,13 +87,14 @@ tr_merge <- alldogs %>%
          "Fav_diff" = "diff") %>%
   select(-c(ra,rs,wins,losses,away))
 
-
 tr_merge <- tr_merge %>% 
   # Pred
   left_join(pred, by = c("home_team" = "Team")) %>% 
-  rename("Home_Pred_Rating" = "Pred_Rating") %>% 
+  rename("Home_Pred_Rating" = "Pred_Rating",
+         "Home_Pred_Rank" = "Pred_Rank") %>% 
   left_join(pred, by = c("away_team" = "Team")) %>% 
-  rename("Away_Pred_Rating" = "Pred_Rating") %>% 
+  rename("Away_Pred_Rating" = "Pred_Rating",
+         "Away_Pred_Rank" = "Pred_Rank") %>% 
   # home
   left_join(hom, by = c("home_team" = "Team")) %>% 
   # away
@@ -145,70 +127,78 @@ tr_merge$DogWinGame_inSeries <- ""
 tr_merge <- tr_merge %>% 
   filter(SeriesGame == 1)
 
+#################################################################
+# Road Dog Criteria: Main UD Picks, DT4, The Dip, and Petricho
+#################################################################
 
-#######################################
-#This only saves the criteria information we're looking to save. Which we found to be worthless because (see notes about the historical Main_Backtest.Rmd) the information is based on after the game was played.
-#Need to also save the model's picks like I did with the NBA System ... began writing the code at the bottom
-#######################################
+### Main ###
+# 1) Start with Road Dogs					
+# 2) Favorite must not have a winning streak of 3 or more games	(FLAG_strk)				
+# 3) Dog must not be on a losing streak of 3 or more games	(FLAG_strk)				
+# 4) Dog must be at least 4-6 in L10 (FLAG_L10) and at least .400 on the road (FLAG_Away)				
+# 5) Favorite must not be better then 6-4 in L10 (FLAG_L10)
+
+### DT-4 ###
+# 1)
+
+### The Dip ###
+# 1)
+# 2)
+
+### Petricho ###
+# 1)
+# 2
+
+#########################
+# Today's System Picks
+#########################
+crit1_today <- tr_merge %>% 
+  mutate( # Decision Tree - 4 rules
+    DT4 = case_when(away_team == Dog & as.numeric(Dog_away_per) > 0.39 & as.numeric(Away_l10_Rating) > -1.55 &
+                      as.numeric(Home_l10_Rating) <= 0.16 & 
+                      (Dog_streak == "losses" & Dog_streakN <= 2) ~ "Met",
+                    .default = "Not-Met"),
+    # Buy the Dip model
+    Dip = case_when(away_team == Dog & as.numeric(Away_Pred_Rating) >= -0.12 & (Dog_streak == "losses" & Dog_streakN <= 1) ~ "Met",
+                    .default = "Not-Met"),
+    Petricho = case_when(
+      # Dog is the away team
+      away_team == Dog &
+        (Fav_streak == "losses" | (Fav_streak == "wins" & Fav_streakN <= 1)) &
+        (Dog_streak == "wins" | (Dog_streak == "losses" & Dog_streakN <= 3)) &
+        as.numeric(Away_Pred_Rank) < 24 &
+        as.numeric(Home_Pred_Rank) - as.numeric(Away_Pred_Rating) < 20 ~ "Met",
+      
+      # Dog is the home team
+      home_team == Dog &
+        (Dog_streak == "losses" | (Dog_streak == "wins" & Dog_streakN <= 1)) &
+        (Fav_streak == "wins" | (Fav_streak == "losses" & Fav_streakN <= 3)) &
+        as.numeric(Home_Pred_Rank) < 24 &
+        as.numeric(Away_Pred_Rank) - as.numeric(Home_Pred_Rating) < 20 ~ "Met",
+      
+      .default = "Not-Met"),
+    # Criteria for my original dog system
+    FLAG_strk = case_when(away_team == Dog & (Dog_streak == "wins" | (Dog_streak == "losses" & Dog_streakN < 3)) &
+                            (Fav_streak == "losses" |(Fav_streak == "wins" & Fav_streakN < 3))  ~ "Met",
+                          .default = "Not-Met"),
+    FLAG_L10 = case_when(away_team == Dog & as.numeric(Dog_L10) >= .400 & as.numeric(Fav_L10) <= .600 ~ "Met",
+                         .default = "Not-Met"),
+    FLAG_Away = case_when(away_team == Dog & as.numeric(Dog_away_per) >= .400 ~ "Met",
+                          .default = "Not-Met"),
+    Main = ifelse(FLAG_strk == "Met" & FLAG_L10 == "Met" & FLAG_Away == "Met" & SeriesGame == 1, "Met", "Not-Met"))
 
 # Cleans data so that it can append nicely to googlesheets
 picks <- crit1_today %>% 
   rename("HomeTeam" = "home_team",
          "AwayTeam" = "away_team") %>% 
-  filter(FLAG_strk == "Met" & FLAG_L10 == "Met" & FLAG_Away == "Met" & SeriesGame == 1) %>% 
-  select(Date, HomeTeam, AwayTeam, GameinSeries:SeriesGame, Dog_price, FLAG_strk:FLAG_Away)
+  filter(DT4 == "Met" | Dip == "Met" | Main == "Met" | Petricho == "Met") %>% 
+  select(Date, HomeTeam, AwayTeam, GameinSeries:SeriesGame, Dog_price, DT4, Dip, Main, Petricho)
 
 # Non_filtered, used especially when picks == 0
 non_filter <- crit1_today %>% 
   rename("HomeTeam" = "home_team",
          "AwayTeam" = "away_team") %>% 
-  select(Date, HomeTeam:SeriesGame, Dog_price, FLAG_strk:FLAG_Away)
-
-#############################################
-# Home Dog - Tracking system with home dogs
-#############################################
-
-# Merge with schedule.R and standings.R
-home_dog <- alldogs %>%
-  left_join(todays_schedule, by = c("home_team" = "HomeTeam")) %>%
-  rename("Dog" = "outcomes_name",
-         "Dog_price" = "outcomes_price",
-         "Date" = "Date.x") %>% 
-  select(Date:away_team, HomeSP:AwayWinPer, Dog, Dog_price) %>% 
-  left_join(team_split, by = c("home_team" = "team")) %>% 
-  rename("Dog_streak" = "streak",
-         "Dog_streakN" = "streak_num",
-         "Dog_away_per" = "away",
-         "Dog_L10" = "lastTen",
-         "Dog_diff" = "diff") %>% 
-  select(Date:Dog_price, Dog_streak, Dog_streakN, Dog_away_per, Dog_L10, Dog_diff) %>% 
-  left_join(team_split, by = c("away_team" = "team")) %>% 
-  rename("Fav_streak" = "streak",
-         "Fav_streakN" = "streak_num",
-         "Fav_home_per" = "home",
-         "Fav_L10" = "lastTen",
-         "Fav_diff" = "diff") %>%
-  select(-c(ra,rs,wins,losses,away)) %>% 
-  filter(home_team == Dog)
-
-# Today's System Picks
-home_dog_criteria <- home_dog %>% 
-  mutate(FLAG_strk = case_when((Dog_streak == "wins" | (Dog_streak == "losses" & Dog_streakN < 3)) &
-                                 (Fav_streak == "losses" |(Fav_streak == "wins" & Fav_streakN < 3))  ~ "Met",
-                               .default = "Not-Met"),
-         FLAG_L10 = case_when(as.numeric(Dog_L10) >= .400 & as.numeric(Fav_L10) <= .600 ~ "Met",
-                              .default = "Not-Met"),
-         FLAG_Away = case_when(as.numeric(Dog_away_per) >= .400 ~ "Met",
-                               .default = "Not-Met"))
-
-home_dog_picks <- home_dog_criteria %>% 
-  rename("HomeTeam" = "home_team",
-         "AwayTeam" = "away_team") %>% 
-  filter(FLAG_strk == "Met" & FLAG_L10 == "Met" & FLAG_Away == "Met" & SeriesGame == 1) %>% 
-  select(Date, HomeTeam, AwayTeam, GameinSeries:SeriesGame, Dog_price, FLAG_strk:FLAG_Away)
-
-
-
+  select(Date, HomeTeam:SeriesGame, Dog_price,  DT4, Dip, Main, Petricho)
 
 ########################
 # GoogleSheets
@@ -243,9 +233,3 @@ googlesheets4::sheet_append(
   sheet = "AllRoadDogs_R"
 )
 
-# Append home dog picks
-googlesheets4::sheet_append(
-  data = home_dog_picks,
-  ss = atp,
-  sheet = "Picks_R_Home"
-)
